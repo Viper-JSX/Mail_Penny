1c851c03bc4451c804a3ed5b5e6c18e79664f0d1
Yura_viper <yuramcpecs@gmail.com>
2022-12-18 07:21:05 +0100

    Added routes for Emails, EmailViewer and EmailEditor

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Outlet, Route, Routes } from "react-router";
 import Header from "./Header/Header";
 
 function Layout(){
@@ -6,8 +6,11 @@ function Layout(){
         <div id="layout">
             <Header />
             <Routes>
-                <Route path="/" element={<h2>Main Page</h2>} exact={true} />
+                <Route path="/" element={<h2>Main Page</h2>} />
                 <Route path="/authorization" element={<h2>Authorization</h2>} />
+                <Route path="/emails" element={<h2>Emails</h2>} />
+                <Route path="/emails/:id" element={<h2>Emails viewer</h2>} />    
+                <Route path="/emails/create" element={<h2>Emails Editor</h2>} />
             </Routes>
         </div>
     )
