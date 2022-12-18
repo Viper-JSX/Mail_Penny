384171ff82f0df7fea42aa6c3cf1befd4ce10a72
Yura_viper <yuramcpecs@gmail.com>
2022-12-18 07:57:16 +0100

    Created action types

---
+++ src/redux/action_types.js
@@ -0,0 +1,8 @@
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_UP = "SIGN_UP";
+export const SIGN_OUT = "SIGN_OUT";
+
+export const CREATE_EMAIL = "CREATE_EMAIL";
+
+export const SHOW_MESSAGE = "SHOW_MESSAGE";
+export const HIDE_MESSAGE = "HIDE_MESSAGE";
