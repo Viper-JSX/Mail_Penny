f413ea36de1f6a464dbd20dd41a63c17345d8824
Yura_viper <yuramcpecs@gmail.com>
2022-12-18 11:54:21 +0100

    Created state that holds email data and created handlers to handle data change (handleRecipientChange, handleSubjectTextChange, handleEmailTextChange)

--- src/components/Email_editor/Email_editor.js
+++ src/components/Email_editor/Email_editor.js
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 function EmailEditor(){
     const user = useSelector((state) => state.user);
+    const [ emailData, setEmailData ] = useState({ 
+        sender: user?.email || "none",
+        recipient: "",
+        subjectText: "",
+        emailText: ""
+    });
+
+
+    function handleRecipientChange(event){
+        setEmailData({ ...emailData, recipient: event.target.value });
+    }
+
+    function handleSubjectTextChange(event){
+        setEmailData({ ...emailData, subjectText: event.target.value });
+    }
+
+    function handleEmailTextChange(event){
+        setEmailData({ ...emailData, emailText: event.target.value });
+    }
 
     return(
         <div className="emailEditor">
             <form className="emailForm" method="POST">
-                <label for="senderInput">Sender
-                    <input id="senderInput" name="sender" value={user?.email || "none"} type="email" disabled />
+                <label htmlFor="senderInput">Sender
+                    <input id="senderInput" name="sender" value={emailData.sender} type="email" disabled />
                 </label>
 
-                <input type="email" name="recipientEmail" placeholder="Recipient" />
-                <input type="text" name="subjectText" placeholder="Subject" />
-                <textarea name="emailText" placeholder="Email text"></textarea>
+                <input type="email" name="recipientEmail" placeholder="Recipient" value={emailData.recipient} onChange={handleRecipientChange} />
+                <input type="text" name="subjectText" placeholder="Subject" value={emailData.subjectText} onChange={handleSubjectTextChange} />
+                <textarea name="emailText" placeholder="Email text" value={emailData.emailText} onChange={handleEmailTextChange}></textarea>
 
                 <button type="submit">Send</button>
             </form>
