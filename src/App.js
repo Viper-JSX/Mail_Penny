8cadf3219fe972bc1ac9e1d9d8fc1a03e8c774da
Yura_viper <yuramcpecs@gmail.com>
2022-12-18 08:36:13 +0100

    Created 'action_creators.js' file which contains action creators, created 'signOut' action creator

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "./components/Layout";
 import { useDispatch } from "react-redux";
 import { createEmail, showMessage, signIn, signUp } from "./redux/thunks";
+import { signOut } from "./redux/action_creators";
 
 function App(){
     const dispatch = useDispatch();
---
+++ src/redux/action_creators.js
@@ -0,0 +1,5 @@
+import { SIGN_OUT } from "./action_types";
+
+export function signOut(){
+    return { type: SIGN_OUT };
+}
