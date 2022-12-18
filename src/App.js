bccfaacb7aaddcaa6cc753713cb295c8e5f04262
Yura_viper <yuramcpecs@gmail.com>
2022-12-18 08:02:04 +0100

    Added cases for each action type related to user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Layout from "./components/Layout";
+import { useDispatch } from "react-redux";
 
 function App(){
+    const dispatch = useDispatch();
+
     return(
         <div className="App">
             <Layout />
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,30 @@
 import { combineReducers } from "redux";
 import { initialState } from "./initial_state";
+import { CREATE_EMAIL, SIGN_IN, SIGN_OUT, SIGN_UP } from "./action_types";
 
 function user(state=initialState.user, action){
     switch(action.type){
-        default:{
-            console.log(state)
+        case SIGN_IN: {
+            console.log("Signing-in");
+            return state;
+        }
+        
+        case SIGN_UP: {
+            console.log("Signing-up");
+            return state;
+        }
+
+        case SIGN_OUT: {
+            console.log("Signing-out");
+            return state; 
+        }
+
+        case CREATE_EMAIL: {
+            console.log("Creating email");
+            return state;
+        }
+
+        default: {
             return state;
         }
     }
