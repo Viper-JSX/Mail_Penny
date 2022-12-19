0291472f8121ac0dbb4b840ab8430cdbd3fa8eaa
Yura_viper <yuramcpecs@gmail.com>
2022-12-19 21:14:53 +0100

    Displayed email's subject, message and recipient

--- src/components/Emails/Email.js
+++ src/components/Emails/Email.js
@@ -1,11 +1,17 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 
 function Email({ handleEmailDelete }){
     const emailId = parseInt(useParams().id);
+    const email = useSelector((state) => state.user?.emails?.results || []).find((email) => email.id === emailId);
+    console.log(email);
 
     return(
         <div className="email">
             <button className="deleteEmailButton" onClick={() => handleEmailDelete({ id: emailId })}>Delete</button>
+            <h3>{email.subject}</h3>
+            <p>{email.message}</p>
+            <b>To: {email.recipient}</b>
         </div>
     );
 }
