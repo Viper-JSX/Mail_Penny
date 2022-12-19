e716f992a8222f0de033418ab95b7a940e3e3b1f
Yura_viper <yuramcpecs@gmail.com>
2022-12-19 15:11:53 +0100

    Added user email adress and username above the emails list

--- src/components/Emails/Emails_top_pannel.js
+++ src/components/Emails/Emails_top_pannel.js
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 function EmailsTopPannel({ handleSignOut }){
+    const user = useSelector((state) => state.user);
+
     return(
         <div>
-            <button className="signOutButton" onClick={handleSignOut}>Sign-out</button>
             <NavLink to="/emails/create">
                 <button>Write email</button>
             </NavLink>
+            <b className="userEmailAdress">{user?.email}</b>
+            <b className="username">{user?.username}</b>
+            <button className="signOutButton" onClick={handleSignOut}>Sign-out</button>
         </div>
     );
 }
