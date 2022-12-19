1101e709eadb18c06b0200be57fa591d2dbc707c
Yura_viper <yuramcpecs@gmail.com>
2022-12-19 11:43:47 +0100

    Created action type for email delete

--- src/redux/action_types.js
+++ src/redux/action_types.js
@@ -3,6 +3,7 @@ export const SIGN_UP = "SIGN_UP";
 export const SIGN_OUT = "SIGN_OUT";
 
 export const SEND_EMAIL = "SEND_EMAIL";
+export const DELETE_EMAIL = "DELETE_EMAIL";
 
 export const SHOW_MESSAGE = "SHOW_MESSAGE";
 export const HIDE_MESSAGE = "HIDE_MESSAGE";
