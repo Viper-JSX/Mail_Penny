e55978df404e5c7d89651c97e0cc255a5f7cda77
Yura_viper <yuramcpecs@gmail.com>
2022-12-20 07:04:27 +0100

    Message text is now displayed as HTML markup

--- src/components/Emails/Email.js
+++ src/components/Emails/Email.js
@@ -1,23 +1,30 @@
+import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router";
 
 function Email({ handleEmailDelete }){
     const user = useSelector((state) => state.user);
+    const messageRef = useRef(null);
+    const emailId = parseInt(useParams().id);
+    const email = useSelector((state) => state.user?.emails?.results || []).find((email) => email.id === emailId);
+
+    useEffect(() => {
+        messageRef.current.innerHTML = email.message;
+    }, [])
+
     if(!user){ //User not signed-in
         return(
             <Navigate to="/authorization" />
         );
     }
 
-    const emailId = parseInt(useParams().id);
-    const email = useSelector((state) => state.user?.emails?.results || []).find((email) => email.id === emailId);
     console.log(email);
 
     return(
         <div className="email">
             <button className="deleteEmailButton" onClick={() => handleEmailDelete({ id: emailId })}>Delete</button>
             <h3>{email.subject}</h3>
-            <p>{email.message}</p>
+            <p ref={messageRef}></p>
             <b>To: {email.recipient}</b>
         </div>
     );
